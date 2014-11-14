05d83e7dc27e27223a13b617631d4a22f0e55d8a
Lukas Juhas <hello@lukasjuhas.com>
2014-11-14 18:00:57 +0000

    1.1 - New Features
    Added Indicator to Admin menu bar
    Added Settings button on plugins page
    Added Default maintenance mode message, if content in settings is empty.

--- lj-maintenance-mode.php
+++ lj-maintenance-mode.php
@@ -3,7 +3,7 @@
  * Plugin Name: Maintenance Mode
  * Plugin URI: https://github.com/lukasjuhas/lj-maintenance-mode
  * Description: Simple Maintenance mode using wordpress die function.
- * Version: 1.0
+ * Version: 1.1
  * Author: Lukas Juhas
  * Author URI: http://lukasjuhas.com
  * Text Domain: lj-maintenance-mode
@@ -26,32 +26,46 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define( 'LJMM_VERSION', '1.0' );
+define( 'LJMM_VERSION', '1.1' );
 define( 'LJMM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'LJMM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-
 
 class ljMaintenanceMode {
 
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'ui' ) );
+		add_action( 'admin_head', array( $this, 'style' ) );
 		add_action( 'admin_init', array( $this, 'settings' ) );
 
 		$is_enabled = get_option('ljmm-enabled');
+
 
 		if($is_enabled) :
 			add_action('get_header', array($this, 'maintenance'));
 		endif;
 		
+		add_action( 'admin_bar_menu', array( $this, 'indicator' ), 100 );
+		add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), array($this, 'action_links') );
+
 	}
 
 	function ui() {
 		add_submenu_page( 'options-general.php', 'Maintenance Mode', 'Maintenance Mode', 'delete_plugins', 'wp-maintenance-mode', array($this, 'settingsPage') );
 	}
 
+	function style() {
+		echo '<style type="text/css">#wp-admin-bar-ljmm-indicator.Enabled { background: rgba(159, 0, 0, 1) }</style>';
+	}
+
 	function settings() {
 		register_setting('ljmm', 'ljmm-enabled');
 		register_setting('ljmm', 'ljmm-content');
+
+		$default = get_option( 'ljmm-content-default');
+		if(empty($default)) :
+			$content = '<h1>Website Under Maintenance</h1><p>Our Website is currently undergoing scheduled maintenance. Please check back soon.</p>';
+			add_option( 'ljmm-content-default', $content);
+		endif;
 	}
 
 	function settingsPage() { ?>
@@ -90,10 +104,37 @@ class ljMaintenanceMode {
 	<?php
 	}
 
+	function indicator($wp_admin_bar) {
+		$is_enabled = get_option('ljmm-enabled');
+		$status = 'Disabled';
+		if($is_enabled)
+			$status = 'Enabled';
+
+		$indicator = array(
+			'id' => 'ljmm-indicator',
+			'title' => 'Maintenance Mode: '.$status,
+			'parent' => false,
+			'href' => get_admin_url(null, 'options-general.php?page=wp-maintenance-mode'),
+			'meta' => array(
+				'title' => 'Maintenance Mode',
+				'class' => $status,
+			)
+		);
+		$wp_admin_bar->add_node($indicator);
+	}
+
+	function action_links( $links ) {
+	   $links[] = '<a href="'. get_admin_url(null, 'options-general.php?page=wp-maintenance-mode') .'">Settings</a>';
+	   return $links;
+	}
+
 	function maintenance() {
 
 		if ( !(current_user_can( 'administrator' ) ||  current_user_can( 'super admin' )) || ( isset($_GET['ljmm']) && $_GET['ljmm'] !== 'preview')) {
 			$content = get_option('ljmm-content');
+			if(empty($content)) {
+				$content = get_option( 'ljmm-content-default');
+			}
 			$content = apply_filters('the_content', $content);
 
 			wp_die($content, 'Maintenance Mode');

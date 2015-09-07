6e474e64b3c3994e2d607926b9bd5ec5f2e475d8
Lukas Juhas <hello@lukasjuhas.com>
2015-09-07 14:57:48 +0100

    :red_circle: disable search engine tracking in progress
    Might need to override current wp_die template with “copy” including no
    follow meta tag as there are no any hooks available.

--- lj-maintenance-mode.php
+++ lj-maintenance-mode.php
@@ -68,6 +68,7 @@ class ljMaintenanceMode {
 
 	 if($is_enabled || isset($_GET['ljmm']) && $_GET['ljmm'] == 'preview') :
 		 add_action('get_header', array($this, 'maintenance'));
+     //add_filter( 'wp_die_handler', array($this, 'wp_die_handler'), 1, 3 );
 	 endif;
 
 	 add_action( 'admin_bar_menu', array( $this, 'indicator' ), 100 );
@@ -97,6 +98,160 @@ class ljMaintenanceMode {
 		 update_option( 'ljmm-content', stripslashes($content));
 	 endif;
  }
+
+ function wp_die_handler( $message, $title = '', $args = array() ) {
+   	$defaults = array( 'response' => 500 );
+  	$r = wp_parse_args($args, $defaults);
+  	$have_gettext = function_exists('__');
+  	if ( function_exists( 'is_wp_error' ) && is_wp_error( $message ) ) {
+  		if ( empty( $title ) ) {
+  			$error_data = $message->get_error_data();
+  			if ( is_array( $error_data ) && isset( $error_data['title'] ) )
+  				$title = $error_data['title'];
+  		}
+  		$errors = $message->get_error_messages();
+  		switch ( count( $errors ) ) {
+  		case 0 :
+  			$message = '';
+  			break;
+  		case 1 :
+  			$message = "<p>{$errors[0]}</p>";
+  			break;
+  		default :
+  			$message = "<ul>\n\t\t<li>" . join( "</li>\n\t\t<li>", $errors ) . "</li>\n\t</ul>";
+  			break;
+  		}
+  	} elseif ( is_string( $message ) ) {
+  		$message = "<p>$message</p>";
+  	}
+  	if ( isset( $r['back_link'] ) && $r['back_link'] ) {
+  		$back_text = $have_gettext? __('&laquo; Back') : '&laquo; Back';
+  		$message .= "\n<p><a href='javascript:history.back()'>$back_text</a></p>";
+  	}
+  	if ( ! did_action( 'admin_head' ) ) :
+  		if ( !headers_sent() ) {
+  			status_header( $r['response'] );
+  			nocache_headers();
+  			header( 'Content-Type: text/html; charset=utf-8' );
+  		}
+  		if ( empty($title) )
+  			$title = $have_gettext ? __('WordPress &rsaquo; Error') : 'WordPress &rsaquo; Error';
+  		$text_direction = 'ltr';
+  		if ( isset($r['text_direction']) && 'rtl' == $r['text_direction'] )
+  			$text_direction = 'rtl';
+  		elseif ( function_exists( 'is_rtl' ) && is_rtl() )
+  			$text_direction = 'rtl';
+  ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php if ( function_exists( 'language_attributes' ) && function_exists( 'is_rtl' ) ) language_attributes(); else echo "dir='$text_direction'"; ?>>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width">
+  <?php wp_no_robots(); ?>
+	<title><?php echo $title ?></title>
+	<style type="text/css">
+		html {
+			background: #f1f1f1;
+		}
+		body {
+			background: #fff;
+			color: #444;
+			font-family: "Open Sans", sans-serif;
+			margin: 2em auto;
+			padding: 1em 2em;
+			max-width: 700px;
+			-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.13);
+			box-shadow: 0 1px 3px rgba(0,0,0,0.13);
+		}
+		h1 {
+			border-bottom: 1px solid #dadada;
+			clear: both;
+			color: #666;
+			font: 24px "Open Sans", sans-serif;
+			margin: 30px 0 0 0;
+			padding: 0;
+			padding-bottom: 7px;
+		}
+		#error-page {
+			margin-top: 50px;
+		}
+		#error-page p {
+			font-size: 14px;
+			line-height: 1.5;
+			margin: 25px 0 20px;
+		}
+		#error-page code {
+			font-family: Consolas, Monaco, monospace;
+		}
+		ul li {
+			margin-bottom: 10px;
+			font-size: 14px ;
+		}
+		a {
+			color: #21759B;
+			text-decoration: none;
+		}
+		a:hover {
+			color: #D54E21;
+		}
+		.button {
+			background: #f7f7f7;
+			border: 1px solid #cccccc;
+			color: #555;
+			display: inline-block;
+			text-decoration: none;
+			font-size: 13px;
+			line-height: 26px;
+			height: 28px;
+			margin: 0;
+			padding: 0 10px 1px;
+			cursor: pointer;
+			-webkit-border-radius: 3px;
+			-webkit-appearance: none;
+			border-radius: 3px;
+			white-space: nowrap;
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing:    border-box;
+			box-sizing:         border-box;
+			-webkit-box-shadow: inset 0 1px 0 #fff, 0 1px 0 rgba(0,0,0,.08);
+			box-shadow: inset 0 1px 0 #fff, 0 1px 0 rgba(0,0,0,.08);
+		 	vertical-align: top;
+		}
+		.button.button-large {
+			height: 29px;
+			line-height: 28px;
+			padding: 0 12px;
+		}
+		.button:hover,
+		.button:focus {
+			background: #fafafa;
+			border-color: #999;
+			color: #222;
+		}
+		.button:focus  {
+			-webkit-box-shadow: 1px 1px 1px rgba(0,0,0,.2);
+			box-shadow: 1px 1px 1px rgba(0,0,0,.2);
+		}
+		.button:active {
+			background: #eee;
+			border-color: #999;
+			color: #333;
+			-webkit-box-shadow: inset 0 2px 5px -3px rgba( 0, 0, 0, 0.5 );
+		 	box-shadow: inset 0 2px 5px -3px rgba( 0, 0, 0, 0.5 );
+		}z
+		<?php if ( 'rtl' == $text_direction ) : ?>
+		body { font-family: Tahoma, Arial; }
+		<?php endif; ?>
+	</style>
+</head>
+<body id="error-page">
+<?php endif; // ! did_action( 'admin_head' ) ?>
+	<?php echo $message; ?>
+</body>
+</html>
+<?php
+	die();
+}
 
  function settingsPage() { ?>
 	 <div class="wrap">
